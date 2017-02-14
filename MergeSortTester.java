4c6e6a5b3ecb13e02b47839de0fe957965700f3a
eric.zhang <ezhang2@stuy.edu>
2017-02-14 11:55:09 -0500

    Added tester

--- MergeSortTester.java
+++ MergeSortTester.java
@@ -13,7 +13,7 @@
   n=10      time: 
   n=100     time: 
   ...
-  n=<huge>  time: 
+
 
   ANALYSIS:
   <INSERT YOUR RESULTS ANALYSIS HERE>
@@ -30,6 +30,36 @@ public class MergeSortTester
      ******************************/
     public static void main( String[] args ) 
     {
+
+	int[] arr1 = new int[1];
+	int[] arr2 = new int[10];
+	int[] arr3 = new int[100];
+	int[] arr4 = new int[1000];
+	int[][] testArray = {arr1,arr2,arr3,arr4};
+	for (int count = 0; count < 4; count++) {
+	    for (int index = 0; index < testArray[count].length; index++) {
+		testArray[count][index] = (int) (Math.random()*10000);
+	    }
+	}
+
+	long t0, timeElapsed;
+	String str = "";
+
+	for (int count = 0; count < 4; count++) {
+	    t0 = System.nanoTime();
+	    testArray[count] = MergeSort.sort(testArray[count]);
+	    timeElapsed = System.nanoTime() - t0;
+	    str += "Length ("+testArray[count].length+"): "+timeElapsed + "\n";
+	}
+	System.out.println(str);
+
+	System.out.print("Test: ");
+	System.out.println(-1*(System.nanoTime()-System.nanoTime()));
+
+	System.out.println(MergeSort.ptestArray[0]);
+	System.out.println(testArray[1]);	
+
+
 
     }//end main
 
